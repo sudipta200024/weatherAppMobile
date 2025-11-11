173d9f58bdf8ed0c7738c9f2618da0802e4af7c6
sudipta1910 <shudiptadasjoy200024@gmail.com>
2025-11-11 23:24:00 +0600

    updated weather history code api

--- lib/Services/weather_api_services.dart
+++ lib/Services/weather_api_services.dart
@@ -17,11 +17,35 @@ class WeatherApiServices {
     }
     final data = json.decode(res.body);
 
-    if(data.containsKey('error')){//containsKey is like a query searching for word error in map
-      throw Exception(data['error']['message']??'Invalid location');
+    if (data.containsKey(
+        'error')) { //containsKey is like a query searching for word error in map
+      throw Exception(data['error']['message'] ?? 'Invalid location');
     }
     return data;
   }
 
   //weather history of past 7 days
+  Future<List<Map<String, dynamic>>> getLast7DaysForecast(String location) async {
+    final List<Map<String, dynamic>> historyWeather = [];
+    final today = DateTime.now();
+    for(int i=1; i<=7; i++){
+      final date = today.subtract(Duration(days: i));
+      final formattedDate = "${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}";
+      final url = Uri.parse("$_baseUrl/history.json?key=$apiKey&q=$location&dt=$formattedDate");
+      final res = await http.get(url);
+      if(res.statusCode != 200){
+        throw Exception("Failed to fetch data : ${res.body}");
+      }
+      final data = json.decode(res.body);
+      if(data.containsKey('error')){
+        throw Exception(data['error']['message'] ?? 'Invalid location');
+      }
+      if(data['forecast']?['forecastday']!=null){
+        historyWeather.add(data);
+      }else{
+        debugPrint("No forecast found for $formattedDate : ${res.body}");
+      }
+    }
+    return historyWeather;
+  }
 }
