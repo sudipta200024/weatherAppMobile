15f3b54517d2cc30845e17864ac548d9afc9c97f
sudipta200024 <shudiptadasjoy200024@gmail.com>
2025-11-30 12:03:22 +0600

    included sharepreference with theme mode

--- lib/Provider/theme_provider.dart
+++ lib/Provider/theme_provider.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod/riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeNotifier extends Notifier<ThemeMode> {
+  static const _key = 'theme_mode';
+
   @override
   ThemeMode build() {
+    _loadTheme();
     return ThemeMode.light;
   }
-  void toggleTheme(){
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final saved = prefs.getString(_key);
+
+    if (saved == 'dark') {
+      state = ThemeMode.dark;
+    } else if (saved == 'light') {
+      state = ThemeMode.light;
+    }
+
+  }
+
+  void toggleTheme() async {
+    // Update UI instantly
     state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+
+    // Save to disk (non-blocking)
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_key, state == ThemeMode.dark ? 'dark' : 'light');
   }
 }
 final themeNotifierProvider = NotifierProvider<ThemeNotifier,ThemeMode>(
